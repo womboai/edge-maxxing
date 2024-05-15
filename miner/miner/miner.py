2b1c364e1b90bec5f922ca8ade95e13f190056bf
Ashley Wright <ashley@msrandom.net>
2024-05-15 18:30:44 +0300

    Fix missing directories

--- miner/miner/miner.py
+++ miner/miner/miner.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from logging import getLogger
+from os import mkdir
 from os.path import isdir, join
 from shutil import copytree, rmtree
 
@@ -93,7 +94,9 @@ def main():
 
     pipeline = pipelines.coreml_sdxl_pipeline
     pipelines.base_minimal_pipeline.save_pretrained(MODEL_DIRECTORY)
+
     rmtree(mlpackages_dir, ignore_errors=True)
+    mkdir(mlpackages_dir)
     copytree(pipelines.coreml_models_path, mlpackages_dir, dirs_exist_ok=True)
 
     comparison = compare_checkpoints(baseline_pipeline, pipeline, expected_average_time)
