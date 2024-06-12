b366d03347d091549e55802cd67dd2e30ad5f6d3
Ashley Wright <ashley@msrandom.net>
2024-06-13 01:43:42 +0300

    Fix __init__ file

--- neuron/neuron/__init__.py
+++ neuron/neuron/__init__.py
@@ -1,4 +1,5 @@
-from .config import *
 from .checkpoint import *
+from .config import *
+from .contest import *
+from .coreml_pipeline import *
 from .random_inputs import *
-from .pipeline import *
