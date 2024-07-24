dd4241079275186165676e67c31e04b1f0188f20
Ashley Wright <ashley@msrandom.net>
2024-07-24 03:13:39 +0300

    Better logging

--- validator/validator/validator.py
+++ validator/validator/validator.py
@@ -204,7 +204,7 @@ class Validator:
                     self.contest_state.miner_info[uid] = None
 
         if not self.should_set_weights:
-            bt.logging.info("Will not set weights as none are present yet")
+            bt.logging.info("Will not set weights as contest is not done")
 
             return
 
