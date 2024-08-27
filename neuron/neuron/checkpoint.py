Use correct energy calculation

neuron/neuron/checkpoint.py
@@ -129,7 +129,7 @@ def get_submission(
 
 def generate(contest: Contest, pipeline: DiffusionPipeline, prompt: str, seed: int) -> GenerationOutput:
     start_vram = contest.get_vram_used(pipeline.device)
-    start_watts = contest.get_watts_used(pipeline.device)
+    start_joules = contest.get_joules(pipeline.device)
     start = perf_counter()
 
     output = pipeline(
@@ -141,7 +141,8 @@ def generate(contest: Contest, pipeline: DiffusionPipeline, prompt: str, seed: i
 
     generation_time = perf_counter() - start
     vram_used = contest.get_vram_used(pipeline.device) - start_vram
-    watts_used = max(0, contest.get_watts_used(pipeline.device) - start_watts)
+    joules_used = contest.get_joules(pipeline.device) - start_joules
+    watts_used = joules_used / generation_time
 
     if isinstance(output, ndarray):
         output = torch.from_numpy(output)
@@ -157,6 +158,7 @@ def generate(contest: Contest, pipeline: DiffusionPipeline, prompt: str, seed: i
 
 
 def compare_checkpoints(contest: Contest, repository: str) -> CheckpointBenchmark:
+    contest.start()
     failed = False
 
     baseline_pipeline = contest.load_baseline()
@@ -261,6 +263,7 @@ def compare_checkpoints(contest: Contest, repository: str) -> CheckpointBenchmar
             f"and watts usage of {watts_used}"
         )
 
+    contest.stop()
     return CheckpointBenchmark(
         baseline_average,
         average_time,

neuron/neuron/contest.py
@@ -29,6 +29,10 @@ class Contest(ABC):
         self.baseline_average = baseline_average
         self.baseline_repository = baseline_repository
 
+    def start(self): pass
+
+    def stop(self): pass
+
     def load_baseline(self):
         return self.load()
 
@@ -52,7 +56,7 @@ class Contest(ABC):
         ...
 
     @abstractmethod
-    def get_watts_used(self, device: torch.device):
+    def get_joules(self, device: torch.device):
         ...
 
     @abstractmethod
@@ -70,7 +74,12 @@ class CudaContest(Contest):
         super().__init__(contest_id, baseline_average, baseline_repository)
 
         self.expected_device_name = expected_device_name
+
+    def start(self):
         pynvml.nvmlInit()
+
+    def stop(self):
+        pynvml.nvmlShutdown()
 
     def load(self, repository: str | None = None):
         return StableDiffusionXLPipeline.from_pretrained(
@@ -87,9 +96,9 @@ class CudaContest(Contest):
     def get_vram_used(self, device: torch.device):
         return torch.cuda.memory_allocated(device)
 
-    def get_watts_used(self, device: torch.device):
+    def get_joules(self, device: torch.device):
         handle = pynvml.nvmlDeviceGetHandleByIndex(device.index)
-        return pynvml.nvmlDeviceGetPowerUsage(handle) / 1000.0
+        return pynvml.nvmlDeviceGetTotalEnergyConsumption(handle) / 1000.0  # convert mJ to J
 
     def validate(self):
         device_name = torch.cuda.get_device_name("cuda")
@@ -108,13 +117,13 @@ class AppleSiliconContest(Contest):
         return CoreMLStableDiffusionXLPipeline.from_pretrained(repository or self.baseline_repository).to("mps")
 
     def get_baseline_size(self):
-        return 0 # TODO
+        return 0  # TODO
 
     def get_vram_used(self, device: torch.device):
         return torch.mps.current_allocated_memory()
 
-    def get_watts_used(self, device: torch.device):
-        return 0 # TODO
+    def get_joules(self, device: torch.device):
+        return 0  # TODO
 
     def validate(self):
         if not torch.backends.mps.is_available():