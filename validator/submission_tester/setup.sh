afaaa6d907bb67ba87f57941ca6042b3ec12a8f5
Ashley Wright <ashley@msrandom.net>
2024-09-06 01:17:35 +0300

    Fix chown not being recursive

--- validator/submission_tester/setup.sh
+++ validator/submission_tester/setup.sh
@@ -8,7 +8,7 @@ chown sandbox:sandbox /sandbox
 
 useradd --create-home --home-dir /home/api api
 
-chown api:api /api
+chown -R api:api /api
 
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y install pipx git
