a53570499930cbe84307a4798419853f1c070957
Alex Nijjar <alexander.nijjar@icloud.com>
2024-09-07 14:57:25 -0500

    Fix wrong directory

--- validator/submission_tester/setup_inference_sandbox.sh
+++ validator/submission_tester/setup_inference_sandbox.sh
@@ -22,7 +22,7 @@ if ! $($BASELINE) || ! [ -f "$READY_MARKER" ]; then
 
   python3.10 -m venv "$VENV"
 
-  "$VENV/bin/pip" install $(cat "$SANDBOX_DIRECTORY/install_args.txt") -e /sandbox
+  "$VENV/bin/pip" install $(cat "$SANDBOX_DIRECTORY/install_args.txt") -e $SANDBOX_DIRECTORY
 
   if $($BASELINE); then
     touch "$READY_MARKER"
