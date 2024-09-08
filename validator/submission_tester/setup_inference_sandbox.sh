4acfd8940945e1fd3f235423725b7c961f376db8
Ashley Wright <ashley@msrandom.net>
2024-09-08 09:49:44 +0300

    Make baseline caching less aggressive

--- validator/submission_tester/setup_inference_sandbox.sh
+++ validator/submission_tester/setup_inference_sandbox.sh
@@ -14,20 +14,21 @@ cd "$SANDBOX_DIRECTORY"
 
 if $($BASELINE) && [ -f "$READY_MARKER" ]; then
   git fetch
-  git checkout "$REVISION"
 else
   rm -rf "$SANDBOX_DIRECTORY/*"
 
   git clone --recursive --no-checkout "$REPOSITORY_URL" "$SANDBOX_DIRECTORY"
 
-  git checkout "$REVISION"
-  git submodule update --init
-
-  python3.10 -m venv "$VENV"
-
-  "$VENV/bin/pip" install $(cat "$SANDBOX_DIRECTORY/install_args.txt") -e $SANDBOX_DIRECTORY
-
   if $($BASELINE); then
     touch "$READY_MARKER"
   fi
 fi
+
+git checkout "$REVISION"
+git submodule update --init
+
+if ! [ -f "$VENV" ]; then
+  python3.10 -m venv "$VENV"
+fi
+
+"$VENV/bin/pip" install $(cat "$SANDBOX_DIRECTORY/install_args.txt") -e $SANDBOX_DIRECTORY
