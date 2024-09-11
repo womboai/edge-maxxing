17aaa69629b3cd1b9e5c27c55dcfb6139cb76665
Alex Nijjar <alexander.nijjar@icloud.com>
2024-09-11 03:48:26 -0500

    Fix docker compose permissions

---
+++ validator/submission_tester/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chown -R baseline-sandbox:baseline-sandbox /baseline-sandbox
+
+exec sudo -u api /home/api/.local/bin/poetry run uvicorn --host 0.0.0.0 --port 8000 submission_tester:app
