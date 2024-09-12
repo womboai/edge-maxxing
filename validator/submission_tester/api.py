cc266e9a329d4f0e44ae800e6d2e5265a6194271
Ashley Wright <ashley@msrandom.net>
2024-09-12 21:49:24 +0300

    Fix websocket send not being properly async(buffer writes, use a future on flush)

--- validator/submission_tester/api.py
+++ validator/submission_tester/api.py
@@ -1,6 +1,7 @@
 import asyncio
+from asyncio import Future, AbstractEventLoop
 from contextlib import asynccontextmanager
-from io import TextIOBase
+from io import TextIOBase, StringIO
 
 from base_validator.metrics import BenchmarkState, BenchmarkResults
 from fastapi import FastAPI, WebSocket, Request
@@ -13,14 +14,42 @@ import logging
 
 class WebSocketLogStream(TextIOBase):
     _websocket: WebSocket
+    _buffer: StringIO
 
-    def __init__(self, websocket: WebSocket):
+    _futures: list[Future[None]]
+    _loop: AbstractEventLoop
+
+    def __init__(self, websocket: WebSocket, loop: AbstractEventLoop):
         super().__init__()
 
         self._websocket = websocket
+        self._buffer = StringIO()
+
+        self._loop = loop
+
+    def __enter__(self):
+        self._buffer.__enter__()
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._buffer.__exit__(exc_type, exc_val, exc_tb)
+
+    def __await__(self):
+        yield from asyncio.gather(*self._futures).__await__()
+
+        self._futures.clear()
+
+    def flush(self):
+        buffered_text = self._buffer.getvalue()
+
+        self._futures.append(asyncio.ensure_future(self._websocket.send_text(buffered_text), loop=self._loop))
+
+        self._buffer.truncate(0)
+        self._buffer.seek(0)
 
     def write(self, text: str):
-        self._websocket.send_text(text)
+        self._buffer.write(text)
 
 
 @asynccontextmanager
@@ -69,13 +98,16 @@ def state(request: Request) -> BenchmarkResults:
 async def stream_logs(websocket: WebSocket):
     await websocket.accept()
 
-    handler = logging.StreamHandler(WebSocketLogStream(websocket))
+    async with WebSocketLogStream(websocket, asyncio.get_running_loop()) as stream:
+        handler = logging.StreamHandler(stream)
 
-    try:
-        logging.root.addHandler(handler)
+        try:
+            logging.root.addHandler(handler)
 
-        while True:
-            await asyncio.sleep(250)
-    finally:
-        # On disconnect
-        logging.root.removeHandler(handler)
+            while True:
+                await asyncio.sleep(500)
+
+                await stream
+        finally:
+            # On disconnect
+            logging.root.removeHandler(handler)
