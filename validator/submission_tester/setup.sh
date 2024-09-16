e686040dffd4fe80e5149c9428597d85ef9449e7
Alex Nijjar <alexander.nijjar@icloud.com>
2024-09-15 19:10:24 -0500

    Add compiling libraries

--- validator/submission_tester/setup.sh
+++ validator/submission_tester/setup.sh
@@ -16,7 +16,7 @@ useradd --create-home --home-dir /home/api api || true
 chown -R api:api /api
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -y install sudo pipx git git-lfs
+DEBIAN_FRONTEND=noninteractive apt-get -y install sudo pipx git git-lfs build-essential python3-dev
 
 su - api -c "
     pipx install poetry;
