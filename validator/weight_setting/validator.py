d748a9b8c6188fa4fce726067e5e518c775973b6
Alex Nijjar <alexander.nijjar@icloud.com>
2024-09-18 11:31:22 -0500

    Revert weight version bump

--- validator/weight_setting/validator.py
+++ validator/weight_setting/validator.py
@@ -46,7 +46,7 @@ from base_validator.metrics import BenchmarkResults, BenchmarkState, CheckpointB
 from .wandb_args import add_wandb_args
 
 VALIDATOR_VERSION = "2.2.7"
-WEIGHTS_VERSION = 27
+WEIGHTS_VERSION = 26
 
 WINNER_PERCENTAGE = 0.8
 IMPROVEMENT_BENCHMARK_PERCENTAGE = 1.05
