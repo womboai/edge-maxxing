fa8b912aaa2426c63c89be6aa51e2a76ca38fe2d
Ashley Wright <ashley@msrandom.net>
2024-09-25 04:24:34 +0300

    Add missing argument types

--- neuron/neuron/config.py
+++ neuron/neuron/config.py
@@ -1,5 +1,6 @@
+import os
 from argparse import ArgumentParser
-from typing import Callable
+from typing import Callable, Any
 
 from fiber.constants import FINNEY_NETWORK
 
@@ -25,6 +26,12 @@ def get_config(add_args: Callable[[ArgumentParser], None] | None = None):
 
     argument_parser.add_argument("--wallet.name", type=str, required=False, help="Wallet name", default="default")
     argument_parser.add_argument("--wallet.hotkey", type=str, required=False, help="Hotkey name", default="default")
+
+    argument_parser.add_argument("--netuid", type=int, required=True, help="Network UID")
+
+    # Deprecated arguments that won't be used
+    argument_parser.add_argument("--logging.debug", action="store_true", help="Enable debug logging", default=False)
+    argument_parser.add_argument("--logging.trace", action="store_true", help="Enable trace logging", default=False)
 
     if add_args:
         add_args(argument_parser)
