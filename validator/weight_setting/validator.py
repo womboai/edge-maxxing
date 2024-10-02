0093ba16fb0d9f3b8979d142ce67959a664a20ca
Alex Nijjar <alexander.nijjar@icloud.com>
2024-10-02 05:24:30 -0500

    Bump validator version

--- validator/weight_setting/validator.py
+++ validator/weight_setting/validator.py
@@ -41,7 +41,7 @@ from neuron import (
 from neuron.submissions import get_submission
 from .wandb_args import add_wandb_args
 
-VALIDATOR_VERSION = "3.0.0"
+VALIDATOR_VERSION = "3.0.1"
 WEIGHTS_VERSION = 30
 
 COLLECTED_SUBMISSIONS_VERSION = 1
