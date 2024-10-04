bc247fda752541c0ac4f95f167ac9b32f47c8c20
Ashley Wright <ashley@wombo.ai>
2024-10-04 23:28:51 +0300

    Improve .netrc mounting (#47)

--- validator/generate_compose.py
+++ validator/generate_compose.py
@@ -34,18 +34,20 @@ VALIDATOR_SERVICE_TEMPLATE = """
       build:
         context: ../
         dockerfile: ./validator/weight_setting/Dockerfile
-      
+
       volumes:
         - ~/.bittensor:/home/validator/.bittensor
-        - ~/.netrc:/home/validator/.netrc
+        - type: bind
+          source: ~/.netrc
+          target: /home/validator/.netrc
 
       environment:
         WANDB_API_KEY: $WANDB_API_KEY
 
       command: --benchmarker_api {apis} $VALIDATOR_ARGS
-      
+
       network_mode: host
-      
+
       depends_on:
 """
 
