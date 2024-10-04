148b7e38637758deaea6131e6ce873a2903fdea4
Ashley Wright <ashley@wombo.ai>
2024-10-04 23:33:59 +0300

    Revert "Improve benchmarking accuracy between validators (#46)" (#48)
    This reverts commit 41fb249d1546406906985fe8230895c4dd1e3f9d.

--- validator/submission_tester/testing.py
+++ validator/submission_tester/testing.py
@@ -96,7 +96,7 @@ def compare_checkpoints(contest: Contest, submission: ModelRepositoryInfo) -> Ch
 
     baseline_outputs: list[GenerationOutput] = []
 
-    min_similarity = 1.0
+    average_similarity = 1.0
 
     with InferenceSandbox(contest.baseline_repository, True) as baseline_sandbox:
         baseline_size = baseline_sandbox.model_size
@@ -129,14 +129,13 @@ def compare_checkpoints(contest: Contest, submission: ModelRepositoryInfo) -> Ch
 
             baseline_outputs.append(baseline)
 
-            if similarity < min_similarity:
-                min_similarity = similarity
+            average_similarity = (average_similarity * i + similarity) / (i + 1)
 
     baseline_average_time = sum(output.generation_time for output in baseline_outputs) / len(baseline_outputs)
     baseline_vram_used = max(output.vram_used for output in baseline_outputs)
     baseline_watts_used = max(output.watts_used for output in baseline_outputs)
 
-    logger.info(f"Similarity Score: {min_similarity}")
+    logger.info(f"Average Similarity: {average_similarity}")
 
     return CheckpointBenchmark(
         baseline=MetricData(
@@ -151,5 +150,5 @@ def compare_checkpoints(contest: Contest, submission: ModelRepositoryInfo) -> Ch
             vram_used=vram_used,
             watts_used=watts_used,
         ),
-        similarity_score=min_similarity,
+        similarity_score=average_similarity,
     )
--- validator/weight_setting/validator.py
+++ validator/weight_setting/validator.py
@@ -42,8 +42,8 @@ from neuron import (
 from neuron.submissions import get_submission
 from .wandb_args import add_wandb_args
 
-VALIDATOR_VERSION = "3.2.0"
-WEIGHTS_VERSION = 32
+VALIDATOR_VERSION = "3.1.0"
+WEIGHTS_VERSION = 31
 
 COLLECTED_SUBMISSIONS_VERSION = 1
 
