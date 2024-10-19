09339cfed035d3163553c61ec453a60f19efd687
Ashley Wright <ashley@msrandom.net>
2024-10-19 04:04:23 +0300

    Fix score used as exponent base being less than 1

--- validator/base_validator/__init__.py
+++ validator/base_validator/__init__.py
@@ -1,3 +1,3 @@
 from .metrics import *
 
-API_VERSION = "4.1.2"
+API_VERSION = "4.1.3"
--- validator/weight_setting/validator.py
+++ validator/weight_setting/validator.py
@@ -50,10 +50,10 @@ from .deduplication import find_duplicates, PotentiallyDuplicateSubmissionInfo
 from .wandb_args import add_wandb_args
 from .winner_selection import get_scores, get_contestant_scores
 
-VALIDATOR_VERSION: tuple[int, int, int] = (4, 1, 2)
+VALIDATOR_VERSION: tuple[int, int, int] = (4, 1, 3)
 VALIDATOR_VERSION_STRING = ".".join(map(str, VALIDATOR_VERSION))
 
-BENCHMARKS_VERSION = 4
+BENCHMARKS_VERSION = 5
 
 WEIGHTS_VERSION = (
     VALIDATOR_VERSION[0] * 10000 +
