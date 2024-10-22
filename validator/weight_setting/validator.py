139cd801c9fcec8e146969a64a1bddd0575b50da
Ashley Wright <ashley@msrandom.net>
2024-10-22 19:28:05 +0300

    Bump version

--- validator/weight_setting/validator.py
+++ validator/weight_setting/validator.py
@@ -49,7 +49,7 @@ from .benchmarking_api import BenchmarkingApi, benchmarking_api
 from .wandb_args import add_wandb_args
 from .winner_selection import get_scores, get_contestant_scores, get_tiers, pick_winners
 
-VALIDATOR_VERSION: tuple[int, int, int] = (4, 2, 0)
+VALIDATOR_VERSION: tuple[int, int, int] = (4, 3, 0)
 VALIDATOR_VERSION_STRING = ".".join(map(str, VALIDATOR_VERSION))
 
 WEIGHTS_VERSION = (
