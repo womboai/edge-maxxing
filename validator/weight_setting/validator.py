0f2945620c9c3887b9a03df5655c862d43306a3c
Ashley Wright <ashley@wombo.ai>
2024-10-25 22:31:49 +0300

    Filter benchmarks without submissions (#76)

--- validator/weight_setting/validator.py
+++ validator/weight_setting/validator.py
@@ -49,7 +49,7 @@ from .benchmarking_api import BenchmarkingApi, benchmarking_api
 from .wandb_args import add_wandb_args
 from .winner_selection import get_scores, get_contestant_scores, get_tiers, get_contestant_tier
 
-VALIDATOR_VERSION: tuple[int, int, int] = (4, 4, 0)
+VALIDATOR_VERSION: tuple[int, int, int] = (4, 4, 1)
 VALIDATOR_VERSION_STRING = ".".join(map(str, VALIDATOR_VERSION))
 
 WEIGHTS_VERSION = (
@@ -835,10 +835,20 @@ class Validator:
 
         for _, result in with_results:
             for hotkey, benchmark in result.results.items():
+                if not hotkey in self.hotkeys:
+                    logger.info(f"{hotkey} not found, skipping")
+                    continue
+
+                uid = self.hotkeys.index(hotkey)
+
+                if not self.contest_state.miner_info[uid]:
+                    logger.info(f"{hotkey} has no submission, skipping")
+                    continue
+
                 if benchmark:
                     logger.info(f"Updating {hotkey}'s benchmarks to {benchmark}")
-                if hotkey in self.hotkeys:
-                    self.benchmarks[self.hotkeys.index(hotkey)] = benchmark
+                self.benchmarks[uid] = benchmark
+
             for hotkey, error_message in result.invalid.items():
                 logger.info(f"Marking {hotkey}'s submission as invalid: '{error_message}'")
                 if hotkey in self.hotkeys:
