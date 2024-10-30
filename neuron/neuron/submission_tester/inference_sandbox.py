92048c4ce4f04ec17db4045f46ac25eef5ada425
Ashley Wright <ashley@msrandom.net>
2024-10-31 02:56:03 +0300

    Add network blocker

--- neuron/neuron/submission_tester/inference_sandbox.py
+++ neuron/neuron/submission_tester/inference_sandbox.py
@@ -12,7 +12,7 @@ from typing import Generic, TypeVar
 
 from pydantic import BaseModel
 
-from .setup_inference_sandbox import setup_sandbox, InvalidSubmissionError
+from .setup_inference_sandbox import setup_sandbox, InvalidSubmissionError, NETWORK_JAIL
 from ..contest import ModelRepositoryInfo
 from ..random_inputs import INFERENCE_SOCKET_TIMEOUT
 
@@ -55,6 +55,7 @@ class InferenceSandbox(Generic[RequestT]):
                 "run",
                 "start_inference",
             ],
+            env={"LD_PRELOAD": NETWORK_JAIL},
             cwd=self._sandbox_directory,
             stdout=PIPE,
             stderr=PIPE,
