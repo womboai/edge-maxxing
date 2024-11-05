9ae870dafee936a23a5338a664e6dd21f4659f90
Ashley Wright <ashley@msrandom.net>
2024-11-05 19:34:56 +0000

    Update all dependencies

--- neuron/neuron/contest.py
+++ neuron/neuron/contest.py
@@ -205,7 +205,7 @@ class ContestDeviceValidationError(Exception):
 CONTESTS = [
     CudaContest(
         ContestId.SDXL_NEWDREAM_NVIDIA_4090,
-        ModelRepositoryInfo(url="https://github.com/womboai/sdxl-newdream-20-inference", revision="f4c5374"),
+        ModelRepositoryInfo(url="https://github.com/womboai/sdxl-newdream-20-inference", revision="84668e6"),
         "NVIDIA GeForce RTX 4090",
     ),
 ]
