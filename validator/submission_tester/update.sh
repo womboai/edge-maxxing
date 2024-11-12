a84003b4821963adcb6d03b3bd72eb28f8a3322d
Ashley Wright <ashley@msrandom.net>
2024-11-12 23:06:58 +0000

    Make update process more flexible

--- validator/submission_tester/update.sh
+++ validator/submission_tester/update.sh
@@ -7,20 +7,18 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install sudo pipx git git-lfs build-es
 
 if ! id -u api &>/dev/null; then
   useradd --shell=/bin/false --create-home --home-dir /home/sandbox sandbox || true
-
-  mkdir -p /sandbox
-
-  chown sandbox:sandbox /sandbox
-
   useradd --create-home --home-dir /home/api api || true
-
   chown -R api:api /api
-
-  echo "api ALL = (sandbox) NOPASSWD: ALL" >> /etc/sudoers
-  echo "Defaults env_keep += \"VALIDATOR_HOTKEY_SS58_ADDRESS VALIDATOR_DEBUG CUDA_VISIBLE_DEVICES\"" >> /etc/sudoers
-
-  git config --system advice.detachedHead false
 fi
+
+mkdir -p /sandbox
+chown sandbox:sandbox /sandbox
+
+echo "root ALL=(ALL:ALL) ALL" > /etc/sudoers
+echo "api ALL = (sandbox) NOPASSWD: ALL" >> /etc/sudoers
+echo "Defaults env_keep += \"VALIDATOR_HOTKEY_SS58_ADDRESS VALIDATOR_DEBUG CUDA_VISIBLE_DEVICES\"" >> /etc/sudoers
+
+git config --system advice.detachedHead false
 
 sudo -u api pipx ensurepath
 sudo -u api pipx install uv
