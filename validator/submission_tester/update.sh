d5a390e1ae0dc33124f1e7832a26180ad49105bb
Ashley Wright <ashley@msrandom.net>
2024-11-18 11:17:39 -0800

    Disable git safe directory check

--- validator/submission_tester/update.sh
+++ validator/submission_tester/update.sh
@@ -24,6 +24,7 @@ echo "root ALL=(ALL:ALL) ALL" > /etc/sudoers
 echo "api ALL = (sandbox) NOPASSWD: ALL" >> /etc/sudoers
 echo "Defaults env_keep += \"VALIDATOR_HOTKEY_SS58_ADDRESS VALIDATOR_DEBUG CUDA_VISIBLE_DEVICES\"" >> /etc/sudoers
 
+git config --system --add safe.directory '*'
 git config --system advice.detachedHead false
 git config --system rebase.autostash true
 git config --system rebase.autosquash true
