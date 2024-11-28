11c06b40b8fe629715bd2cc7138f83d039fe1edd
Ashley Wright <ashley@msrandom.net>
2024-11-28 12:14:58 -0800

    Fix docker

--- validator/weight_setting/start.sh
+++ validator/weight_setting/start.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-~/.local/bin/uv run opentelemetry-instrument \
+~/.local/bin/uv run --no-dev --frozen opentelemetry-instrument \
   --service_name edge-maxxing-validator \
   --exporter_otlp_endpoint http://98.81.78.238:4317 \
   --resource_attributes "neuron.type=validator,$(~/.local/bin/uv run python3 telemetry_attributes.py "$@")" \
