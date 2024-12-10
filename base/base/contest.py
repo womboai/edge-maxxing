fecd72eff22a18f0b77df0c5fdb810651819357a
Ashley Wright <ashley@msrandom.net>
2024-12-10 12:19:13 -0800

    Remove CPU metric for now

--- base/base/contest.py
+++ base/base/contest.py
@@ -26,7 +26,6 @@ class MetricType(IntEnum):
     WATTS_USED = 4
     LOAD_TIME = 5
     RAM_USED = 6
-    CPU_USED = 7
 
 
 class ContestId(IntEnum):
@@ -105,8 +104,6 @@ class Contest:
             normalize(baseline.vram_used, benchmark.metrics.vram_used, MetricType.VRAM_USED),
             normalize(baseline.watts_used, benchmark.metrics.watts_used, MetricType.WATTS_USED),
             normalize(baseline.load_time, benchmark.metrics.load_time, MetricType.LOAD_TIME),
-            normalize(baseline.ram_used, benchmark.metrics.ram_used, MetricType.RAM_USED),
-            normalize(baseline.cpu_used, benchmark.metrics.cpu_used, MetricType.CPU_USED),
         ])
 
         return score * similarity * metric_weights.get(MetricType.SIMILARITY_SCORE, 0) / total_weight
