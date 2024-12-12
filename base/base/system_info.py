47f7bf916c2d1248766408449dfac24d8a8df1b7
Ashley Wright <ashley@msrandom.net>
2024-12-12 11:29:50 -0800

    Add ram and ssd speed system info

--- base/base/system_info.py
+++ base/base/system_info.py
@@ -1,4 +1,12 @@
+import mmap
+import os
+import shutil
+from pathlib import Path
+from time import perf_counter
+
+import numpy as np
 import psutil
+from cachetools import cached, TTLCache
 from pydantic import BaseModel
 
 
@@ -10,9 +18,16 @@ class SystemInfo(BaseModel):
     physical_cores: int
     total_cores: int
     ram: int
+    ram_speed: float
     gpu: str
+    total_storage: int
+    used_storage: int
+    free_storage: int
+    ssd_read_speed: float
+    ssd_write_speed: float
 
 
+@cached(cache=TTLCache(maxsize=1, ttl=300))
 def get_system_info() -> SystemInfo:
     with open("/proc/cpuinfo", "r") as f:
         for line in f:
@@ -33,6 +48,9 @@ def get_system_info() -> SystemInfo:
     import torch
     gpu = torch.cuda.get_device_name(0)
 
+    disk_usage = shutil.disk_usage("/")
+    read_speed, write_speed = calculate_ssd_speed()
+
     return SystemInfo(
         cpu=cpu,
         min_frequency_mhz=min_frequency,
@@ -41,5 +59,51 @@ def get_system_info() -> SystemInfo:
         physical_cores=physical_cores,
         total_cores=total_cores,
         ram=ram,
+        ram_speed=calculate_ram_speed(),
         gpu=gpu,
+        total_storage=disk_usage.total,
+        used_storage=disk_usage.used,
+        free_storage=disk_usage.free,
+        ssd_read_speed=read_speed,
+        ssd_write_speed=write_speed,
     )
+
+
+@cached(cache=TTLCache(maxsize=1, ttl=86400))
+def calculate_ram_speed() -> float:
+    size = 1024 * 1024 * 100
+    a = np.random.random(size // 8)
+    b = np.random.random(size // 8)
+    np.random.random(size // 8)
+    start = perf_counter()
+    c = a + b
+    c.sum()
+    end = perf_counter()
+    return (size * 3) / (end - start)
+
+
+@cached(cache=TTLCache(maxsize=1, ttl=86400))
+def calculate_ssd_speed() -> tuple[float, float]:
+    test_file = Path("speedtest.tmp")
+    chunk_size = 1024 * 1024
+    total_size = chunk_size * 10
+
+    try:
+        start = perf_counter()
+        with open(test_file, "wb") as f:
+            f.write(b"\0" * total_size)
+            f.flush()
+            os.fsync(f.fileno())
+        end = perf_counter()
+        write_speed = total_size / (end - start)
+
+        with open(test_file, "rb") as f:
+            with mmap.mmap(f.fileno(), total_size, access=mmap.ACCESS_READ) as mm:
+                start = perf_counter()
+                mm.read(total_size)
+                end = perf_counter()
+        read_speed = total_size / (end - start)
+
+        return read_speed, write_speed
+    finally:
+        test_file.unlink()
