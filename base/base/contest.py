f9993268a171e25492fa8fa31a9111eb55dfb61e
Ashley Wright <ashley@msrandom.net>
2024-12-13 03:36:18 +0300

    Bump revision

--- base/base/contest.py
+++ base/base/contest.py
@@ -134,7 +134,7 @@ CONTESTS = [
         contest_id=ContestId.FLUX_NVIDIA_4090,
         device=CUDA_4090_DEVICE,
         output_comparator=partial(ImageOutputComparator, CUDA_4090_DEVICE),
-        baseline_repository=RepositoryInfo(url="https://github.com/womboai/flux-schnell-edge-inference", revision="5248bb3"),
+        baseline_repository=RepositoryInfo(url="https://github.com/womboai/flux-schnell-edge-inference", revision="1d254c6"),
     ),
     Contest(
         contest_id=ContestId.SDXL_NEWDREAM_NVIDIA_4090,
