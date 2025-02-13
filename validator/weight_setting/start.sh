43c9eba07614c95f2d9afbe42b419fa5168a54c7
Ashley Wright <ashley@msrandom.net>
2025-02-14 01:05:23 +0300

    Run uv sync everytime

--- validator/weight_setting/start.sh
+++ validator/weight_setting/start.sh
@@ -7,6 +7,8 @@ git config --local rebase.autostash true
 git config --local rebase.autosquash true
 git config --local pull.autostash true
 
+~/.local/bin/uv sync --no-dev --frozen --prerelease=allow
+
 ~/.local/bin/uv run --no-dev --frozen opentelemetry-instrument \
   --service_name edge-maxxing-validator \
   --exporter_otlp_endpoint http://98.81.78.238:4317 \
